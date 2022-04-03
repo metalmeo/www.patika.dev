5410f9dfd819f9c8442dff0976b91c00178e98fb
metalmeo <guloglu.mehmet@yandex.com.tr>
2022-04-03 22:35:45 +0300

    Update sql_train6.sql

--- sql_train6.sql
+++ sql_train6.sql
@@ -1,4 +1,4 @@
 select avg(rental_rate) from film;
-count (film) from film where film like 'C%';
+count (*) from film where film like 'C%';
 select MAX(length) from film where rental_rate=0.99;
 count (distinct replacement_cost) where length >150;
