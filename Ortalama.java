6de7dc647b11afbd59b8c09fcdb1aad2c3ca5a1b
metalmeo <guloglu.mehmet@yandex.com.tr>
2022-04-17 21:37:24 +0300

    Update Ortalama.java

--- Ortalama.java
+++ Ortalama.java
@@ -1,31 +1,27 @@
 import java.util.Scanner;
 
-class ortalama{
-  public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);  // Create a Scanner object
+class ortalama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // Create a Scanner object
 
-        int mat_not, kimya_not,turkce_not,Tarih_not,muzik_not ;
+        float mat_not, kimya_not, turkce_not, Tarih_not, muzik_not;
         float not_ortalama;
 
         System.out.println("Matematik Notu?");
-        mat_not=input.nextInt();
+        mat_not = input.nextFloat();
         System.out.println("kimya Notu?");
-        kimya_not=input.nextInt();
+        kimya_not = input.nextFloat();
         System.out.println("Turkce Notu?");
-        turkce_not=input.nextInt();
+        turkce_not = input.nextFloat();
         System.out.println("Tarih Notu?");
-        Tarih_not=input.nextInt();
+        Tarih_not = input.nextFloat();
         System.out.println("Muzik Notu?");
-        muzik_not=input.nextInt();
-    
+        muzik_not = input.nextFloat();
 
-        not_ortalama= (mat_not+kimya_not+turkce_not+Tarih_not+muzik_not)/5;
+        not_ortalama = (mat_not + kimya_not + turkce_not + Tarih_not + muzik_not) / 5;
+        String durum = (not_ortalama > 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println("Not Ortalamasi = " + not_ortalama + " " + durum);
 
-    
+    }
 
-    System.out.println("Not Ortalamasi = " + not_ortalama);
-
-  }
-   
-  
 }
