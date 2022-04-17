c8a6cd07f7639772ca9d9575e88325327d49ce88
metalmeo <guloglu.mehmet@yandex.com.tr>
2022-04-17 23:16:30 +0300

    Create taksimetre.java

---
+++ taksimetre.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class taksimetre {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double km, km_tutar, toplam_tutar;
+        double indi_bindi = 20;
+        km_tutar = 2.20;
+
+        System.out.println("Kilometreyi girin");
+
+        km = input.nextInt();
+        toplam_tutar = 0;
+
+        if (toplam_tutar < 20) {
+            toplam_tutar = 20;
+        } else {
+            toplam_tutar = km * km_tutar;
+        }
+
+        System.out.println("ödeme" + toplam_tutar);
+    }
+
+}
